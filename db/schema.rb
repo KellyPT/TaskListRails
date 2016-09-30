311a30db8ba310b4016befcb1f35df664d4781e8
Kelly Tran <trankphuong@gmail.com>
2016-09-30 14:51:34 -0700

    Removed 'name' field in my model and database.

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,9 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160930210427) do
+ActiveRecord::Schema.define(version: 20160930214226) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name"
     t.string   "description"
     t.boolean  "completion_status"
     t.datetime "created_at",        null: false
