76215bb8d6d2d8df55a2fc1293d49ae459141aea
Kelly Tran <trankphuong@gmail.com>
2016-10-24 00:52:03 -0700

    Create yml fixturs for User model. Added Tasks Controller Tests for authentication.

--- test/controllers/tasks_controller_test.rb
+++ test/controllers/tasks_controller_test.rb
@@ -1,7 +1,23 @@
 require 'test_helper'
 
 class TasksControllerTest < ActionController::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "not-logged-in user should get redirected from pages to homepage" do
+    session.delete(:user_id)
+    get :index
+    assert_response :redirect
+    assert_redirected_to root_path
+  end
+
+  test "Logged in user can see exactly her pages" do
+    session[:user_id] = users(:ada).id
+    get :index
+    assert_response :success
+    assert_template "tasks/index"
+    tasks_from_controller = assigns(:tasks)
+    assert_equal tasks_from_controller.length, users(:ada).tasks.length
+
+    tasks_from_controller.each do |task|
+      assert_includes users(:ada).tasks, task
+    end
+  end
 end
