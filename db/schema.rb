1d22b4e9f27abee1e2d67898ce38f07b63d4d29a
Kelly Tran <trankphuong@gmail.com>
2016-10-23 22:35:52 -0700

    Begin Phase 3. Modify the Task Model to create a relation to the User Model. A User should have many tasks and each Task should belong to a User.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161017220628) do
+ActiveRecord::Schema.define(version: 20161024053051) do
 
   create_table "tasks", force: :cascade do |t|
     t.string   "description"
@@ -20,7 +20,10 @@ ActiveRecord::Schema.define(version: 20161017220628) do
     t.datetime "updated_at",        null: false
     t.string   "title"
     t.datetime "completed_at"
+    t.integer  "user_id"
   end
+
+  add_index "tasks", ["user_id"], name: "index_tasks_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "name"
